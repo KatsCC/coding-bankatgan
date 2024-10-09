2eadf4850ab9c2a80cf63958be0e9f066f0dae5f
GeunYoung <162268787+KatsCC@users.noreply.github.com>
2024-10-09 12:50:42 +0900

    🚀 배포용 msw on

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { worker } from './mocks/browser.ts';
+import { worker } from './mocks/browser.ts';
 
 const queryClient = new QueryClient();
 
-// await worker.start({
-//   onUnhandledRequest: 'bypass',
-// });
+await worker.start({
+  onUnhandledRequest: 'bypass',
+});
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
